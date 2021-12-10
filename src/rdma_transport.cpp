9e777611a59119476677ef71d85cc72eb8be178d
xinpingdeng <xinping.deng@gmail.com>
2021-12-10 13:16:37 +1100

    done the easy part, get functions into pybind11

--- src/rdma_transport.cpp
+++ src/rdma_transport.cpp
@@ -1,6 +1,7 @@
 #include "rdma_transport.hpp"
 extern "C" {
 #include "RDMAapi.h"
+#include "RDMAexchangeidcallbacks.h"
 }
 
 #include <stdio.h>
@@ -57,7 +58,34 @@ struct RdmaTransport {
 			  union ibv_gid *gidAddressPtr,
 			  int *gidIndexPtr,
 			  enum ibv_mtu *mtuPtr);
+  
+  enum exchangeResult exchangeViaStdIO(bool isSendMode,
+				       uint32_t packetSequenceNumber,
+				       uint32_t queuePairNumber,
+				       union ibv_gid gidAddress,
+				       uint16_t localIdentifier,
+				       uint32_t *remotePSNPtr,
+				       uint32_t *remoteQPNPtr,
+				       union ibv_gid *remoteGIDPtr,
+				       uint16_t *remoteLIDPtr);
 
+  int modifyQueuePairReady(struct ibv_qp *queuePair,
+			   uint8_t rdmaPort,
+			   int gidIndex,
+			   enum runMode mode,
+			   int packetSequenceNumber,
+			   uint32_t remotePSN,
+			   uint32_t remoteQPN,
+			   union ibv_gid remoteGID,
+			   uint16_t remoteLID,
+			   enum ibv_mtu mtu);
+  
+  int registerMemoryRegions(struct ibv_pd *protectionDomain,
+			    MemoryRegionManager* manager);
+
+  int ibv_req_notify_cq(struct ibv_cq *cq,
+			int solicited_only);
+  
   int addition(int a, int b){
     return a+b;
   }
@@ -85,6 +113,34 @@ PYBIND11_MODULE(rdma_transport, m) {
                       uint8_t ,
                       uint32_t ,
                       uint32_t >())
+      
+      /**********************************************************************
+       * Sets up the RDMA connection and return its details in the
+       * pointer parameters
+       **********************************************************************/
+      .def("setupRDMAConnection", &RdmaTransport::setupRDMAConnection)
+      
+      /**********************************************************************
+       * Exchanges the necessary RDMA configuration identifiers with those of
+       * the remote RDMA peer, by printing the local values to the display
+       * and prompting the user to enter the remote values
+       * Returns EXCH_SUCCESS if identifier exchange has completed successfully
+       **********************************************************************/
+      .def("exchangeViaStdIO", &RdmaTransport::exchangeViaStdIO)
+      
+      /**********************************************************************
+       * Modifies the queue pair so that it is ready to receive
+       * and possibly to also send
+       **********************************************************************/
+      .def("modifyQueuePairReady", &RdmaTransport::modifyQueuePairReady)
+
+      /**********************************************************************
+       * Registers memory regions with protection domain
+       **********************************************************************/
+      .def("registerMemoryRegions", &RdmaTransport::registerMemoryRegions)
+
+      .def("ibv_req_notify_cq", &RdmaTransport::ibv_req_notify_cq)
+       
       .def("say_hello", &RdmaTransport::say_hello)
       .def("addition", &RdmaTransport::addition);
     
